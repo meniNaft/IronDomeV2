1402e89ee4c2c984853d7934bf852ac9ef063412
oriori770 <oriori770@gmail.com>
2024-08-11 11:59:46 +0300

    VMVolley

---
+++ ViewModel/VMVolley.cs
@@ -0,0 +1,10 @@
+﻿using IronDomeV2.Models;
+
+namespace IronDomeV2.ViewModel
+{
+    public class VMVolley
+    {
+        public int AttackerId { get; set; }
+        public IEnumerable<MethodOfAttack> MethodsOfAttack { get; set; }
+    }
+}
